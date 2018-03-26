af49cefa5ed182e2553ab79e4fe99b1431ebac0a
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-03-26 09:45:48 -0400

    Added tooltip

--- js/main.js
+++ js/main.js
@@ -3,9 +3,43 @@ const innerCircleRadius = 50;
 const SVG_NS = "http://www.w3.org/2000/svg";
 const dataElem = document.getElementById( "data" );
 const loaderElem = document.getElementById( "loader" );
+const tipElem = document.getElementById( "tooltip" );
 const showOffset = true;
 const dataFile = "/data/final-data-opt-500rec.min.json";
 // const dataFile = "/data/final-data-opt-140862rec.min.json";
+const tipData = {
+	"name": "Name",
+	"category_name": "Category",
+	"launch_date": "Launched on",
+	"goal": "Goal",
+	"pledged": "Pledged",
+	"perc_pledged": "% pledged",
+	"score": "Sentiment score",
+};
+
+
+// ...
+let tipInfo = null;
+Object.keys( tipData ).forEach( ( k ) => {
+	tipInfo = document.createElement( "div" );
+	tipInfo.classList.add( "row" );
+	tipInfo.innerHTML = `
+		<div class="col col--right"><p>${ tipData[ k ] }:</p></div>
+		<div class="col"><p><span id="${ k }">x</span></p></div>
+	`;
+
+	tipElem.appendChild( tipInfo );
+});
+let showTip = ( e ) => {
+	let currInfo = JSON.parse( e.target.getAttribute( "data-info" ) );
+	Object.keys( tipData ).forEach( ( k ) => {
+		$( "#" + k ).text( currInfo[ k ] );
+	});
+	tipElem.classList.remove( "hide" );
+};
+let hideTip = ( e ) => {
+	tipElem.classList.add( "hide" );
+};
 
 
 // ...
@@ -21,10 +55,14 @@ $.getJSON( dataFile, ( data, msg ) => {
 		pointElem = document.createElementNS( SVG_NS, "circle" );
 		pointElem.setAttribute( "cx", 250 );
 		pointElem.setAttribute( "cy", 250 );
-		pointElem.setAttribute( "r", 0.7 );
-		pointElem.classList.add( "path", "path--filled" );
+		pointElem.setAttribute( "r", 2 );
+		pointElem.setAttribute( "data-info", JSON.stringify( d ) );
+		pointElem.classList.add( "path", "path--point" );
 
-		monthIdx = window.parseInt( d.launch_date.substr( 5, 2 ) );
+		pointElem.addEventListener( "mouseover", showTip );
+		pointElem.addEventListener( "mouseleave", hideTip );
+
+		monthIdx = window.parseInt( d.launch_date.substr( 5, 2 ) ) - 1;
 		offsetAngle = ( monthIdx / 12 * 360 ) + ( showOffset ? d.score * 15 : 0 );
 
 		pointElem.setAttribute( "transform", `
@@ -36,7 +74,7 @@ $.getJSON( dataFile, ( data, msg ) => {
 		dataElem.appendChild( pointElem );
 	}
 
-	loaderElem.remove();
+	loaderElem.classList.add( "hide" );
 });
 
 
