c4c34818982db757104c3262a78b44a6fdb5a956
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-04-02 16:18:32 -0400

    Added functionality to anchor the visualization about the selected month

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@
 const pointRadius = 1.5;
 const innerCircleRadius = 50;
 const SVG_NS = "http://www.w3.org/2000/svg";
+const svgElem = document.getElementById( "main" );
 const dataElem = document.getElementById( "data" );
 const loaderElem = document.getElementById( "loader" );
 const tipElem = document.getElementById( "tooltip" );
 const filtersElem = document.getElementById( "filters" );
 const offsetCheckElem = document.getElementById( "showOffsetCheck" );
+const monthSelectorElem = document.getElementById( "monthSelector" );
+const activeMonthElem = document.getElementById( "activeMonth" );
 const dataFile = "data/final-data-opt-2000rec.min.json";
 const tipData = {
     "name": "Name",
@@ -17,21 +20,18 @@ const tipData = {
     "perc_pledged": "% pledged",
     "score": "Sentiment score",
 };
+const months = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"
+];
 
 
 // ...
-let showOffset = offsetCheckElem.value;
+let categories = {};
+let showOffset = offsetCheckElem.checked;
+let monthToAnchor = +monthSelectorElem.value;
 let tipInfo = null;
-Object.keys( tipData ).forEach( ( k ) => {
-    tipInfo = document.createElement( "div" );
-    tipInfo.classList.add( "row" );
-    tipInfo.innerHTML = `
-        <div class="col col--right"><p>${ tipData[ k ] }:</p></div>
-        <div class="col"><p><span id="${ k }">x</span></p></div>
-    `;
-
-    tipElem.appendChild( tipInfo );
-});
 let showTip = ( e ) => {
     let currInfo = JSON.parse( e.target.getAttribute( "data-info" ) );
     Object.keys( tipData ).forEach( ( k ) => {
@@ -42,34 +42,10 @@ let showTip = ( e ) => {
 let hideTip = ( e ) => {
     tipElem.classList.add( "hide" );
 };
-
-
-// ...
-let categories = {};
 let getCategoryClassName = ( s ) => "cat--" + s.replace( " ", "-" ).toLowerCase();
 
 
 // ...
-const request = new XMLHttpRequest();
-request.open( "GET", dataFile, true );
-
-request.onload = () => {
-    if( request.status >= 200 && request.status < 400 ) {
-        // Success!
-        let startTime = Date.now();
-        loadVIz( JSON.parse( request.responseText ) );
-        console.log( "Time taken:\n" + ( ( Date.now() - startTime ) / 1000 ) + " seconds" )
-    } else {
-        // We reached our target server, but it returned an error
-        console.log( "An error occurred..." )
-    }
-};
-
-request.onerror = function() {
-    console.log( "Connection error occurred..." )
-};
-
-request.send();
 let loadVIz = ( data, msg ) => {
     let d = null;
     let pointElem = null;
@@ -88,7 +64,7 @@ let loadVIz = ( data, msg ) => {
             continue;
         }
 
-        monthIdx = window.parseInt( d.launch_date.substr( 5, 2 ) ) - 1;
+        monthIdx = +d.launch_date.substr( 5, 2 ) - 1;
         offsetAngle = showOffset ? d.score * 15 : 0;
         monthAngle = ( monthIdx / 12 * 360 );
         percPledged = Math.round( d.perc_pledged );
@@ -101,7 +77,7 @@ let loadVIz = ( data, msg ) => {
             pointElem.setAttribute( "r", pointRadius );
             pointElem.setAttribute( "data-info", JSON.stringify( d ) );
             pointElem.setAttribute( "data-month-angle", monthAngle );
-            pointElem.setAttribute( "data-offset-angle", offsetAngle );
+            pointElem.setAttribute( "data-offset-angle", d.score * 15 );
             pointElem.setAttribute( "data-perc-pledged", percPledged );
             pointElem.setAttribute( "transform", `
                 rotate(${ window.isNaN( monthAngle + offsetAngle ) ? 0 : monthAngle + offsetAngle } 250,250)
@@ -142,15 +118,15 @@ let loadVIz = ( data, msg ) => {
 };
 
 
+// ...
 let toggleSentimentOffset = () => {
     showOffset = !showOffset;
 
     let ma = oa = pp = null;
-
     Array.prototype.forEach.call( document.getElementsByClassName( "path--point" ), ( pt ) => {
-        ma = window.parseInt( pt.getAttribute( "data-month-angle"  ), 10 );
-        oa = showOffset ? window.parseInt( pt.getAttribute( "data-offset-angle" ), 10 ) : 0;
-        pp = window.parseInt( pt.getAttribute( "data-perc-pledged" ), 10 );
+        ma = +pt.getAttribute( "data-month-angle"  );
+        oa = showOffset ? +pt.getAttribute( "data-offset-angle" ) : 0;
+        pp = +pt.getAttribute( "data-perc-pledged" );
 
         pt.setAttribute( "transform", `
             rotate(${ window.isNaN( ma + oa ) ? 0 : ma + oa } 250,250)
@@ -158,5 +134,57 @@ let toggleSentimentOffset = () => {
             ` );
     });
 };
+let anchorVizToSelectedMonth = () => {
+    activeMonthElem.innerHTML = months[ +monthSelectorElem.value ];
+    svgElem.setAttribute( "transform", `rotate(${ ( +monthSelectorElem.value / 12 * -360 ) - 90 } 250,250)` );
+};
+
+
+// ...
+Object.keys( tipData ).forEach( ( k ) => {
+    tipInfo = document.createElement( "div" );
+    tipInfo.classList.add( "row" );
+    tipInfo.innerHTML = `
+        <div class="col col--right"><p>${ tipData[ k ] }:</p></div>
+        <div class="col"><p><span id="${ k }">x</span></p></div>
+    `;
+
+    tipElem.appendChild( tipInfo );
+});
+
+months.forEach( ( m, i ) => {
+    let monthOption = document.createElement( "option" );
+    monthOption.value = i;
+    monthOption.innerText = m;
+    monthSelectorElem.appendChild( monthOption );
+});
 
 offsetCheckElem.addEventListener( "change", toggleSentimentOffset );
+monthSelectorElem.addEventListener( "change", anchorVizToSelectedMonth );
+
+anchorVizToSelectedMonth();
+
+
+// ...
+const request = new XMLHttpRequest();
+request.open( "GET", dataFile, true );
+
+request.onload = () => {
+    if( request.status >= 200 && request.status < 400 ) {
+        // Success!
+        let startTime = Date.now();
+        loadVIz( JSON.parse( request.responseText ) );
+        console.log( "Time taken:\n" + ( ( Date.now() - startTime ) / 1000 ) + " seconds" )
+    } else {
+        // We reached our target server, but it returned an error
+        console.log( "An error occurred..." )
+    }
+};
+
+request.onerror = function() {
+    console.log( "Connection error occurred..." )
+};
+
+request.send();
+
+
