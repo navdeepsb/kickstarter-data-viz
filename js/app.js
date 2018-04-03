fd18c4cf5938ba16c487b7620f0ae9f560e45448
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-04-03 08:19:06 -0400

    Refactored and updated categorical filtering

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@ const months = [
 
 // ...
 let categories = {};
+let categoriesToShow = [];
+let pointElems = [];
 let showOffset = offsetCheckElem.checked;
 let monthToAnchor = +monthSelectorElem.value;
 let tipInfo = null;
@@ -46,75 +48,92 @@ let getCategoryClassName = ( s ) => "cat--" + s.replace( " ", "-" ).toLowerCase(
 
 
 // ...
-let loadVIz = ( data, msg ) => {
-    let d = null;
+let getPointToRender = ( datum ) => {
+    let pointElem   = null;
+    let thisCatg    = getCategoryClassName( datum.category_name );
+    let monthIdx    = +datum.launch_date.substr( 5, 2 ) - 1;;
+    let monthAngle  = ( monthIdx / 12 * 360 );
+    let offsetAngle = showOffset ? datum.score * 15 : 0;
+    let percPledged = Math.round( datum.perc_pledged );
+
+    pointElem = document.createElementNS( SVG_NS, "circle" );
+    pointElem.setAttribute( "cx", 250 );
+    pointElem.setAttribute( "cy", 250 );
+    pointElem.setAttribute( "r", pointRadius );
+    pointElem.setAttribute( "data-info", JSON.stringify( datum ) );
+    pointElem.setAttribute( "data-month-angle", monthAngle );
+    pointElem.setAttribute( "data-offset-angle", datum.score * 15 );
+    pointElem.setAttribute( "data-perc-pledged", percPledged );
+    pointElem.setAttribute( "data-cat", thisCatg );
+    pointElem.setAttribute( "transform", `
+        rotate(${ window.isNaN( monthAngle + offsetAngle ) ? 0 : monthAngle + offsetAngle } 250,250)
+        translate(${ innerCircleRadius + percPledged } 0)
+        ` );
+    pointElem.addEventListener( "mouseover", showTip );
+    pointElem.addEventListener( "mouseleave", hideTip );
+    pointElem.classList.add( "path", "path__point", thisCatg, `path__${ thisCatg }` );
+
+    pointElems.push( pointElem );
+
+    return pointElem;
+};
+
+let updatePointsToRender = () => {
     let pointElem = null;
-    let monthIdx = null;
-    let monthAngle = null;
-    let offsetAngle = null;
-    let percPledged = null;
-    let thisCatg = null;
-    let currCatg = location.href.split( "filter=" )[ 1 ];
 
-    for( let i = 0, len = data.length; i < len; i++ ) {
-        d = data[ i ];
-        thisCatg = getCategoryClassName( d.category_name );
+    for( let j = pointElems.length - 1; j >= 0; j-- ) {
+        pointElem = pointElems[ j ];
 
-        if( currCatg && ( currCatg !== thisCatg ) ) {
-            continue;
-        }
-
-        monthIdx = +d.launch_date.substr( 5, 2 ) - 1;
-        offsetAngle = showOffset ? d.score * 15 : 0;
-        monthAngle = ( monthIdx / 12 * 360 );
-        percPledged = Math.round( d.perc_pledged );
-
-        try {
-            pointElem = document.createElementNS( SVG_NS, "circle" );
-            pointElem.classList.add( "path", "path--point", thisCatg );
-            pointElem.setAttribute( "cx", 250 );
-            pointElem.setAttribute( "cy", 250 );
-            pointElem.setAttribute( "r", pointRadius );
-            pointElem.setAttribute( "data-info", JSON.stringify( d ) );
-            pointElem.setAttribute( "data-month-angle", monthAngle );
-            pointElem.setAttribute( "data-offset-angle", d.score * 15 );
-            pointElem.setAttribute( "data-perc-pledged", percPledged );
-            pointElem.setAttribute( "transform", `
-                rotate(${ window.isNaN( monthAngle + offsetAngle ) ? 0 : monthAngle + offsetAngle } 250,250)
-                translate(${ innerCircleRadius + percPledged } 0)
-                ` );
-            pointElem.addEventListener( "mouseover", showTip );
-            pointElem.addEventListener( "mouseleave", hideTip );
-
-            dataElem.appendChild( pointElem );
-
-            // Aggregate categories:
-            if( !categories.hasOwnProperty( d.category_name ) ) {
-                categories[ d.category_name ] = 0;
-            }
-            categories[ d.category_name ] += 1;
-
-        } catch( ex ) {
-            console.log( "Exception occurred!" );
-            console.log( ex.toString() );
+        // Decide whether to show/hide this data point on the basis
+        // of categories filter:
+        pointElem.classList.remove( "path__point--hidden" );
+        if( categoriesToShow.indexOf( pointElem.getAttribute( "data-cat" ) ) < 0 ) {
+            pointElem.classList.add( "path__point--hidden" );
         }
     }
+};
 
-    loaderElem.innerText = "Hover over each project to view details...";
-    console.log( categories );
+let loadVIz = ( data ) => {
 
-    // Show filters:
+    for( let i = 0, len = data.length; i < len; i++ ) {
+        let d = data[ i ];
+        let c = d.category_name;
+
+        // Append the data point to DOM:
+        dataElem.appendChild( getPointToRender( d ) );
+
+        // Aggregate categories:
+        if( !categories.hasOwnProperty( c ) ) {
+            categories[ c ] = 0;
+        }
+        ++categories[ c ];
+    }
+
+    // Make filters:
+    let ct = null;
     let li = null;
-    let link = null;
     Object.keys( categories ).forEach( ( c ) => {
+        ct = getCategoryClassName( c );
         li = document.createElement( "li" );
-        link = document.createElement( "a" );
-        link.setAttribute( "href", "?filter=" + getCategoryClassName( c ) )
-        link.innerText = c + " (" + categories[ c ] + ")";
-        link.classList.add( "filter", getCategoryClassName( c ) );
-        li.appendChild( link );
+        li.classList.add( "filter", ct );
+        li.innerHTML = `
+            <input type="checkbox" id="${ ct }" checked="checked" data-cat="${ ct }" />
+            <label for="${ ct }">${ c } (${ categories[ c ] })</label>
+        `;
+
+        li.querySelector( "input[type=checkbox]" ).addEventListener( "change", () => {
+            updateCurrCategories();
+            updatePointsToRender();
+        });
+
         filtersElem.appendChild( li );
     });
+
+    // And update current categories initially:
+    updateCurrCategories();
+
+    // Hide the loader:
+    loaderElem.innerText = "Hover over each project to view details...";
 };
 
 
@@ -123,7 +142,7 @@ let toggleSentimentOffset = () => {
     showOffset = !showOffset;
 
     let ma = oa = pp = null;
-    Array.prototype.forEach.call( document.getElementsByClassName( "path--point" ), ( pt ) => {
+    Array.prototype.forEach.call( document.getElementsByClassName( "path__point" ), ( pt ) => {
         ma = +pt.getAttribute( "data-month-angle"  );
         oa = showOffset ? +pt.getAttribute( "data-offset-angle" ) : 0;
         pp = +pt.getAttribute( "data-perc-pledged" );
@@ -137,6 +156,12 @@ let toggleSentimentOffset = () => {
 let anchorVizToSelectedMonth = () => {
     activeMonthElem.innerHTML = months[ +monthSelectorElem.value ];
     svgElem.setAttribute( "transform", `rotate(${ ( +monthSelectorElem.value / 12 * -360 ) - 90 } 250,250)` );
+};
+let updateCurrCategories = () => {
+    categoriesToShow = [];
+    Array.prototype.forEach.call( document.querySelectorAll( ".filter input[type=checkbox]" ), ( c ) => {
+        if( c.checked ) categoriesToShow.push( c.getAttribute( "data-cat" ) );
+    });
 };
 
 
